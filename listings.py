50d68d31e2dda5e98840a6b6bd85c188bc76a4c9
ssbabu95 <babu.sumeet1@gmail.com>
2015-11-15 15:57:45 -0500

    Supposed to be continuously checking

--- listings.py
+++ listings.py
@@ -1,11 +1,13 @@
 from kafka import KafkaConsumer
 from elasticsearch import Elasticsearch
 import json
+
 consumer = KafkaConsumer('new-listings-topic', group_id='listing-indexer', bootstrap_servers=['kafka:9092'])
 es = Elasticsearch(['es'])
 
-for message in consumer:
-	new_listing = json.loads(message.value.decode())
-	es.index(index='listing_index', doc_type='listing', id=new_listing['creator'], body=new_listing)
-	es.indices.refresh(index="listing_index")
-	print(new_listing)
+while True:
+	for message in consumer:
+		new_listing = json.loads(message.value.decode())
+		es.index(index='listing_index', doc_type='listing', id=new_listing['id'], body=new_listing)
+		es.indices.refresh(index="listing_index")
+		#print(new_listing)
