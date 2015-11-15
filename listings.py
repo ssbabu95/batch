73351410be72c16d47303e5e4a8becd8d0eecd0f
alexander-goossens <ag4wj@virginia.edu>
2015-11-15 19:48:42 +0000

    add listings to kafka

---
+++ listings.py
@@ -0,0 +1,11 @@
+from kafka import KafkaConsumer
+from elasticsearch import Elasticsearch
+import json
+consumer = KafkaConsumer('new-listings-topic', group_id='listing-indexer', bootstrap_servers=['kafka:9092'])
+es = Elasticsearch(['es'])
+
+for message in consumer:
+	new_listing = json.loads(message.value.decode())
+	es.index(index='listing_index', doc_type='listing', id=new_listing['creator'], body=new_listing)
+	es.indices.refresh(index="listing_index")
+	print(new_listing)
